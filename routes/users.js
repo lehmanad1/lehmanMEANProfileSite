6f43289eaeac601a204997ce751c273524e4b168
Andrew Lehman <lehmanad1@live.com>
2017-07-05 23:49:05 -0400

    initial commit for storage

---
+++ routes/users.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const config = require('../config/database');
+const User = require('../models/user');
+
+// Register
+router.post('/register', (req, res, next) => {
+  let newUser = new User({
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  User.addUser(newUser, (err, user) => {
+    if(err){
+      res.json({success: false, msg: err});
+    } else {
+      res.json({success: true, msg:'User registered'});
+    }
+  });
+});
+
+// Authenticate
+router.post('/authenticate', (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.getUserByEmail(email, (err, user) => {
+    if(err) throw err;
+    if(!email){
+      return res.json({success: false, msg: email});
+    }
+    if(!user){
+      return res.json({success: false, msg: 'Email not found'});
+    }
+
+    User.comparePassword(password, user.password, (err, isMatch) => {
+      if(err) throw err;
+      if(isMatch){
+        const token = jwt.sign(user, config.secret, {
+          expiresIn: 604800 // 1 week
+        });
+
+        res.json({
+          success: true,
+          token: 'JWT '+token,
+          user: {
+            id: user._id,
+            firstname: user.firstname,
+            lastname: user.lastname,
+            username: user.username,
+            email: user.email
+          }
+        });
+      } else {
+        return res.json({success: false, msg: 'Wrong password'});
+      }
+    });
+  });
+});
+
+// Profile
+router.get('/profile', passport.authenticate('jwt', {session:false}), (req, res, next) => {
+  res.json({user: req.user});
+});
+
+module.exports = router;
